c7fe005dcf20c9cecc562d25c618475f8fde5794
Joney Pereira <joney.pereira.ext@conductor.com.br>
2022-06-28 19:24:15 -0300

    Criado o recurso Users

---
+++ src/main/java/com/example/curso/resources/UsersResources.java
@@ -0,0 +1,20 @@
+package com.example.curso.resources;
+
+import com.example.curso.entities.Users;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UsersResources {
+
+    @GetMapping
+    public ResponseEntity<Users> findAll(){
+
+        Users u = new Users(1L, "Maria", "maria@maria.com", "81996953403", "@1234");
+
+        return ResponseEntity.ok().body(u);
+    }
+}
