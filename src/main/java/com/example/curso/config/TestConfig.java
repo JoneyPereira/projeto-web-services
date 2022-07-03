bd44f66f15ee2c15d947177d61ed7afa5862415a
Joney Pereira <joney.pereira.ext@conductor.com.br>
2022-07-03 08:54:38 -0300

    Criado o pagamento do pedido.

--- src/main/java/com/example/curso/config/TestConfig.java
+++ src/main/java/com/example/curso/config/TestConfig.java
@@ -1,14 +1,8 @@
 package com.example.curso.config;
 
-import com.example.curso.entities.Category;
-import com.example.curso.entities.Order;
-import com.example.curso.entities.Product;
-import com.example.curso.entities.User;
+import com.example.curso.entities.*;
 import com.example.curso.entities.enums.OrderStatus;
-import com.example.curso.repositories.CategoryRepository;
-import com.example.curso.repositories.OrderRepository;
-import com.example.curso.repositories.ProductRepository;
-import com.example.curso.repositories.UserRepository;
+import com.example.curso.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -33,6 +27,9 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private ProductRepository productRepository;
 
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
 
@@ -40,13 +37,14 @@ public class TestConfig implements CommandLineRunner {
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
 
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
 		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
 		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
 		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 
-		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
 		p1.getCategories().add(cat2);
@@ -58,7 +56,6 @@ public class TestConfig implements CommandLineRunner {
 
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
-
 		User u1 = new User(null, "Maria Brown", "maria@maria.com", "81996953403", "@1234");
 		User u2 = new User(null, "Alex Green", "alex@alex", "82996953403", "@1234");
 		User u3 = new User(null, "Jhon Kill", "jhon@jhon", "83996953403", "@1234");
@@ -67,9 +64,21 @@ public class TestConfig implements CommandLineRunner {
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1);
 
-
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
+
 
 	}
 }
